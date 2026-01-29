302e3825f52e2df80ee20a66074848b679a370da
Ian Yoo <isyoo@ucdavis.edu>
2026-01-29 15:40:02 -0800

    add initial session calls

--- apps/cli/main.cc
+++ apps/cli/main.cc
@@ -1,45 +1,27 @@
-/**
- * #include <api/render_session.h>
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    // 1. Parse Args (Hardcoded for now)
-    int width = 400;
-    int height = 225;
-    std::string outfile = "test_render.ppm";
-
-    // 2. Start Session
-    api::RenderSession session;
-    session.Init(width, height);
-
-    // 3. Render
-    session.Render();
-
-    // 4. Save
-    session.Save(outfile);
-
-    return 0;
-}
-
-
+#include <session/render_session.h>
 
 #include <iostream>
 #include <string>
 
-#include "session/render_session.h"
+int main(int argc, char *argv[])
+{
+    // Parse Args (Hardcoded for now)
+    int width = 400;
+    int height = 225;
+    std::string outfile = "test_render.ppm";
 
-int main(int argc, char* argv[]) {
-  // Hardcoded for Phase 1
-  const int width = 400;
-  const int height = 225;
-  const std::string outfile = "test_render.ppm";
+    // Start a rendering instance (Session)
+    skwr::RenderSession session;
 
-  skwr::RenderSession session;
+    // session.LoadScene();
 
-  session.Init(width, height);
-  session.Render();
-  session.Save(outfile);
+    session.SetOptions(width, height, 1);
 
-  return 0;
+    // Render
+    session.Render();
+
+    // Save
+    session.Save(outfile);
+
+    return 0;
 }
- */
