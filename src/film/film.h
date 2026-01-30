f5a75a642e61f7729697451ff4852b824ba729e0
Ian Yoo <isyoo@ucdavis.edu>
2026-01-29 17:12:55 -0800

    add pixel struct and comments

--- src/film/film.h
+++ src/film/film.h
@@ -7,6 +7,31 @@
 namespace skwr
 {
 
+    // These structs will likely be moved to their own headers later.
+    // Here initially for simplicity
+
+    struct Pixel
+    {
+        // TODO: RGB and alpha. should be able to accumulate color like RTIOW does it
+        Spectrum color_sum = Spectrum(); // Accumulated Radiance
+        float alpha_sum = 0.f;           // Accumulated Opacity
+        float weight_sum = 0.f;          // Total weight (filter weight * count)
+
+        // // Helper to get the final averaged value
+        // Spectrum GetAverageColor() const
+        // {
+        //     if (weight_sum == 0)
+        //         return Spectrum();
+        //     return color_sum / weight_sum;
+
+        //     // we need weight_sum instead of something like int sample_count
+        //     // Anti-Aliasing: In high-quality rendering, a ray that hits the center of a pixel
+        //     // is worth more (1.0) than a ray that hits the edge (0.1).
+        //     // This is called "Pixel Filtering" (Gaussian or Mitchell-Netravali filters).
+        //     // FinalColor= ∑(SampleColor×FilterWeight)/∑FilterWeight
+        // }
+    };
+
     struct DeepPixel
     {
         // TODO: Add list of samples {depth, color, transmittance} here
@@ -29,7 +54,7 @@ namespace skwr
 
     private:
         int width_, height_;
-        std::vector<Spectrum> pixels_;       // The flat image
+        std::vector<Pixel> pixels_;          // The flat image
         std::vector<DeepPixel> deep_pixels_; // The deep image buffer
     };
 
