a847c1f423aeaea8170b6b90034064c52ca1189a
Ian Yoo <isyoo@ucdavis.edu>
2026-01-29 16:54:56 -0800

    add namespace and temp alias for color3f - spectrum

---
+++ src/core/spectrum.h
@@ -0,0 +1,21 @@
+#ifndef SKWR_CORE_SPECTRUM_H_
+#define SKWR_CORE_SPECTRUM_H_
+
+#include "core/color3f.h"
+
+// Spectrum is an abstraction for color3f.
+// Spectrum represents the distribution of light energy (RADIANCE) across wavelengths
+// Color3f represents the human-visible RGB perception
+//
+
+namespace skwr
+{
+
+    // CONFIGURATION:
+    // For now, we simulate light using RGB values.
+    // In the future, we can change this ONE line to switch to Spectral Rendering (8 wavelengths).
+    using Spectrum = Color3f;
+
+} // namespace skwr
+
+#endif // SKWR_CORE_SPECTRUM_H_
