c6e0cb4603533480285d3eefb5d15ba5c5fdf870
C3viche <28328104+C3viche@users.noreply.github.com>
2026-01-31 13:35:53 -0800

    add image naming to cli

--- apps/cli/main.cc
+++ apps/cli/main.cc
@@ -3,12 +3,27 @@
 #include <iostream>
 #include <string>
 
+void print_usage(const char* program_name)
+{
+    std::cerr << "Usage: " << program_name << "\n";
+    std::cerr << "       " << program_name << " --name outfile.ppm\n";
+}
+
 int main(int argc, char *argv[])
 {
     // Parse Args (Hardcoded for now)
+    if (argc != 3 && argc != 1)
+    {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    std::string outfile = "test_render.ppm";
+    if (argc == 3 && strcmp(argv[1], "--name") == 0) // make name argument
+        outfile = argv[2];
+
     int width = 400;
     int height = 225;
-    std::string outfile = "test_render.ppm";
 
     // Start a rendering instance (Session)
     skwr::RenderSession session;
