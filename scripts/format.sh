cda6eb171f38b3a62ec1e1223ca24f4e9776d3ff
C3viche <28328104+C3viche@users.noreply.github.com>
2026-01-31 17:09:36 -0800

    make script look for all files from root

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-find src -name '*.cpp' -o -name '*.h' | xargs clang-format -i -style=file
-
 # Grabs the directory this script is in, then goes up one level
 cd "$(dirname "$0")/.." || exit
 
@@ -15,6 +13,6 @@ echo "Formatting code..."
 
 # uses parentheses \( \) to group extensions correctly
 # uses -print0 and -0 to handle filenames with spaces safely
-find src \( -name '*.cpp' -o -name '*.h' \) -print0 | xargs -0 clang-format -i -style=file
+find . \( -name "*.cc" -o -name "*.cpp" -o -name "*.h" \) -print0 | xargs -0 clang-format -i -style=file
 
 echo "Done."
