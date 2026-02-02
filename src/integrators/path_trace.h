fb92a63ec4d97948048bebf562c196284089c35a
Ian Yoo <isyoo@ucdavis.edu>
2026-02-01 23:36:51 -0800

    [feature] Normals: add normal integrator rendering

---
+++ src/integrators/path_trace.h
@@ -0,0 +1,14 @@
+#ifndef SKWR_INTEGRATORS_PATH_TRACE_H_
+#define SKWR_INTEGRATORS_PATH_TRACE_H_
+
+#include "integrators/integrator.h"
+
+namespace skwr {
+
+class PathTrace : public Integrator {
+  public:
+    void Render(const Scene& scene, const Camera& cam, Film* film) override;
+};
+}  // namespace skwr
+
+#endif  // SKWR_INTEGRATORS_PATH_TRACE_H_
