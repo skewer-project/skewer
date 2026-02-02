c50b4f7cf7c8f30f8fd3ed1f594d00424310d085
Ian Yoo <isyoo@ucdavis.edu>
2026-02-01 20:56:34 -0800

    [refactor] SurfaceInteraction: hit_record now 'fat' robust struct

---
+++ src/scene/surface_interaction.h
@@ -0,0 +1,28 @@
+#ifndef SKWR_SCENE_SURFACE_INTERACTION_H_
+#define SKWR_SCENE_SURFACE_INTERACTION_H_
+
+#include "core/ray.h"
+#include "core/vec3.h"
+
+namespace skwr {
+
+// "Surface Interaction" is basically a beefed up HitRecord
+// It's a "fat" data struct that's calculated immediately
+struct SurfaceInteraction {
+    Point3 p;         // Exact point of intersection
+    Vec3 n;           // Surface normal (geometric)
+    Float t;          // Distance along the ray
+    bool front_face;  // Is normal pointing at ray? (Is it the outside face?)
+
+    // Future: UVs, dpdu, dpdv, Material*
+
+    // Helper to align normal against the incoming ray
+    inline void SetFaceNormal(const Ray& r, const Vec3& outward_normal) {
+        front_face = Dot(r.direction(), outward_normal) < 0;
+        n = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+}  // namespace skwr
+
+#endif  // SKWR_SCENE_SURFACE_INTERACTION_H_
