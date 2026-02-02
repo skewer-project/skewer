eb81dd6566d5e066e64c793207b0dbfb16729f46
C3viche <28328104+C3viche@users.noreply.github.com>
2026-02-01 22:18:36 -0800

    separate ImageBuffer into Deep and Flat

--- src/film/image_buffer.h
+++ src/film/image_buffer.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <ImfArray.h>
 
 #include "core/spectrum.h"
 
@@ -22,6 +23,57 @@ class ImageBuffer {
     int width_;
     int height_;
     std::vector<Spectrum> pixels_;
+};
+
+struct DeepSample {
+    // Depth information
+    float z_front;
+    float z_back; // for volumes. Can just be z_front for hard surfaces
+    Spectrum color;
+    float transmittance;
+};
+
+struct DeepPixel {
+    std::vector<DeepSample> samples;
+
+    void Sort(); // OpenEXR expects samples to be sorted by depth
+};
+
+class DeepImageBuffer {
+    public:
+      DeepImageBuffer(int width, int height);
+
+      // Set a pixel's color (0,0 is top-left usually)
+      void SetPixel(int x, int y, const DeepPixel &p);
+
+    private:
+      int width_;
+      int height_;
+      std::vector<DeepPixel> pixels_; // Contiguous memory but allows [x][y] indexing
+
+      // NOTE: might have to change from array of structures (DeepPixel) to structure of arrays
+      // std::vector<half> z_;
+      // std::vector<half> r_;
+      // std::vector<half> g_;
+      // std::vector<half> b_;
+      // std::vector<half> a_;
+
+};
+
+class FlatImageBuffer {
+    public:
+      FlatImageBuffer(int width, int height) : width_(width), height_(height), pixels_({}) {};
+
+      FlatImageBuffer(int width, int height, std::vector<Spectrum> pixels)
+        : width_(width), height_(height), pixels_(pixels) {};
+
+      // Set a pixel's color (0,0 is top-left usually)
+      void SetPixel(int x, int y, const Spectrum &s);
+
+    private:
+      int width_;
+      int height_;
+      std::vector<Spectrum> pixels_;
 };
 
 }  // namespace skwr
