23e0684dfa80e8c47314a21778e81dccc3fc3e89
Ian Yoo <isyoo@ucdavis.edu>
2026-02-03 22:27:13 -0800

    [feature] Material: create dod material struct

--- src/materials/material.h
+++ src/materials/material.h
@@ -1,3 +1,31 @@
+#ifndef SKWR_MATERIALS_MATERIAL_H_
+#define SKWR_MATERIALS_MATERIAL_H_
+
+#include <cstdint>
+
+#include "core/spectrum.h"
+
+namespace skwr {
+
+enum class MaterialType : uint8_t { Lambertian, Metal, Dielectric };
+
+// 32-byte aligned to fit in cache?
+struct Material {
+    MaterialType type;
+
+    // padding to align data types
+    uint8_t _padding[3];
+
+    // Data params
+    Spectrum albedo;  // Color (Diffuse or Specular)
+    Float roughness;  // 0.0 = Perfect Mirror, 1.0 = Matte
+    Float ior;        // Index of refraction
+};
+
+}  // namespace skwr
+
+#endif  // SKWR_MATERIALS_MATERIAL_H_
+
 // #ifndef MATERIAL_H
 // #define MATERIAL_H
 // //==============================================================================================
