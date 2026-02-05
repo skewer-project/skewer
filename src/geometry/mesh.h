e75364c3ed195589003f34bddeca25beecb59f74
Ian Yoo <isyoo@ucdavis.edu>
2026-02-04 16:35:17 -0800

    [feature] Mesh: add triangle mesh implementation

---
+++ src/geometry/mesh.h
@@ -0,0 +1,24 @@
+#ifndef SKWR_GEOMETRY_MESH_H_
+#define SKWR_GEOMETRY_MESH_H_
+
+#include <cstdint>
+#include <vector>
+
+#include "core/vec3.h"
+
+namespace skwr {
+
+struct Mesh {
+    // Structure of Arrays style data
+    std::vector<Vec3> p;  // Positions
+    std::vector<Vec3> n;  // Normals (optional, could be empty)
+    // std::vector<Vec3> uv // Texture coords
+
+    // Index buffer
+    std::vector<uint32_t> indices;
+    uint32_t material_id;
+};
+
+}  // namespace skwr
+
+#endif  // SKWR_GEOMETRY_MESH_H_
