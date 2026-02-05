79a01a80867786615936ea40034912af8baf56b3
Ian Yoo <isyoo@ucdavis.edu>
2026-02-04 22:53:52 -0800

    [feature] Ray: add inverse direction optimization

--- src/core/ray.h
+++ src/core/ray.h
@@ -12,10 +12,16 @@ class Ray {
     // Ray(const Point3& origin, const Vec3& direction, double time)
     //     : orig(origin), dir(direction), tm(time) {}
 
-    Ray(const Point3& origin, const Vec3& direction) : orig_(origin), dir_(direction) {}
+    Ray(const Point3& origin, const Vec3& direction) : orig_(origin), dir_(direction) {
+        // Precompute inverse optimization
+        // IEEE 754 floating point handles 1.0/0.0 as Infinity, which works
+        // correctly with the slab method intersection logic.
+        inv_dir = Vec3(1.0f / direction.x(), 1.0f / direction.y(), 1.0f / direction.z());
+    }
 
     const Point3& origin() const { return orig_; }
     const Vec3& direction() const { return dir_; }
+    const Vec3& inv_direction() const { return inv_dir; }
     // double time() const { return tm_; }
 
     // 3D pos (P) on Ray is function of P(t) = A + tb, A = origin, b = Ray direction
@@ -24,6 +30,7 @@ class Ray {
   private:
     Point3 orig_;
     Vec3 dir_;
+    Vec3 inv_dir;
     // double tm_;
 };
 
