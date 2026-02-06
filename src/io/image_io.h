e54f752f0a4b88181e1f4ea5414aa7ed5001467d
C3viche <28328104+C3viche@users.noreply.github.com>
2026-02-06 14:45:06 -0800

    remove ChannelInfo and MutablePixelView structs

--- src/io/image_io.h
+++ src/io/image_io.h
@@ -11,17 +11,11 @@ class ImageIO {
   public:
     static void SavePPM(const FlatImageBuffer& buf, const std::string filename);
 
-    static void SaveEXR(DeepImageBuffer& buf, const std::string filename);
+    static void SaveEXR(const DeepImageBuffer& buf, const std::string filename);
 
     static FlatImageBuffer LoadPPM(const std::string filename);
 
     static DeepImageBuffer LoadEXR(const std::string filename);
-};
-
-struct ChannelInfo {
-    const char* name;
-    bool required;
-    bool present;
 };
 
 }  // namespace skwr
