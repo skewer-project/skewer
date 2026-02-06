aeae61aa4c34ca6f80048d46f67647a3a7b9d0f4
Ian Yoo <isyoo@ucdavis.edu>
2026-02-06 14:42:37 -0800

    [feature] SurfaceInteraction: add w out field

--- src/scene/surface_interaction.h
+++ src/scene/surface_interaction.h
@@ -6,6 +6,8 @@
 #include "core/ray.h"
 #include "core/vec3.h"
 
+/* TODO: Implement Deferred Differential Geometry */
+
 namespace skwr {
 
 // "Surface Interaction" is basically a beefed up HitRecord
@@ -13,6 +15,7 @@ namespace skwr {
 struct SurfaceInteraction {
     Point3 p;         // Exact point of intersection
     Vec3 n;           // Surface normal (geometric)
+    Vec3 wo;          // Outgoing direction (points to Camera/viewer)
     Float t;          // Distance along the ray
     bool front_face;  // Is normal pointing at ray? (Is it the outside face?)
     uint32_t material_id;
@@ -21,6 +24,7 @@ struct SurfaceInteraction {
 
     // Helper to align normal against the incoming ray
     inline void SetFaceNormal(const Ray& r, const Vec3& outward_normal) {
+        wo = -Normalize(r.direction());
         front_face = Dot(r.direction(), outward_normal) < 0;
         n = front_face ? outward_normal : -outward_normal;
     }
