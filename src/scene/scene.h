979105ac15649f2c820fc35f0d92ab3658905f67
Ian Yoo <isyoo@ucdavis.edu>
2026-02-07 00:57:52 -0800

    [refactor] Scene: reorder header and .cc code

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -24,59 +24,18 @@ class Scene {
   public:
     Scene() = default;
 
-    // void AddShape(const Shape &shape);
-
-    // Returns the index of the added sphere (for debugging rn)
-    uint32_t AddSphere(const Sphere &s) {
-        spheres_.push_back(s);
-        const Material &mat = materials_[s.material_id];
-        if (mat.IsEmissive()) {
-            // Create the Light Wrapper
-            AreaLight light;
-            light.type = AreaLight::Sphere;
-            light.primitive_index = (uint32_t)spheres_.size() - 1;
-            light.emission = mat.emission;
-
-            // 4. Add to the "Cheat Sheet"
-            lights_.push_back(light);
-        }
-        return static_cast<uint32_t>(spheres_.size() - 1);
-    }
-
-    uint32_t AddMaterial(const Material &m) {
-        materials_.push_back(m);
-        return static_cast<uint32_t>(materials_.size() - 1);
-    }
+    uint32_t AddSphere(const Sphere &s);
+    uint32_t AddMaterial(const Material &m);
+    uint32_t AddMesh(Mesh &&m);  // Returns mesh_id (index in the meshes_ vector)
 
     const Material &GetMaterial(uint32_t id) const { return materials_[id]; }
-
-    // Returns mesh_id (index in the meshes_ vector)
-    uint32_t AddMesh(Mesh &&m) {
-        meshes_.push_back(std::move(m));
-        uint32_t mesh_id = (uint32_t)meshes_.size() - 1;
-
-        // AUTO-GENERATE TRIANGLES
-        // When we add a mesh, we immediately break it into Triangle primitives
-        // so the renderer can see them.
-        const Mesh &mesh_ref = meshes_.back();
-        for (size_t i = 0; i < mesh_ref.indices.size(); i += 3) {
-            Triangle t;
-            t.mesh_id = mesh_id;
-            t.v_idx = (uint32_t)i;  // Points to the first index of the triplet
-            triangles_.push_back(t);
-        }
-
-        return mesh_id;
-    }
-
-    // We need to look up meshes by ID during intersection
     const Mesh &GetMesh(uint32_t id) const { return meshes_[id]; }
+    const std::vector<Sphere> &Spheres() const { return spheres_; }
+    const std::vector<Triangle> &Triangles() const { return triangles_; }
+    const std::vector<Material> &Materials() const { return materials_; }
+    const std::vector<AreaLight> &Lights() const { return lights_; }
 
     void Build();  // Construct the BVH from the shapes list
-
-    // THE CRITICAL HOT-PATH FUNCTION
-    // The Integrator calls this millions of times.
-    // rn loops through linearly, but when BVH is implemented, should be faster
     bool Intersect(const Ray &r, Float t_min, Float t_max, SurfaceInteraction *si) const;
     bool IntersectBVH(const Ray &r, Float t_min, Float t_max, SurfaceInteraction *si) const;
 
