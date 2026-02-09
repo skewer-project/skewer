fa7e8163bd4b9f12ce0300fa1507615b62a2fb92
C3viche <28328104+C3viche@users.noreply.github.com>
2026-02-09 15:49:50 -0800

    some bug fixes and optimizations

--- src/io/image_io.h
+++ src/io/image_io.h
@@ -9,13 +9,13 @@ namespace skwr {
 
 class ImageIO {
   public:
-    static void SavePPM(const FlatImageBuffer& buf, const std::string filename);
+    static void SavePPM(const FlatImageBuffer& buf, const std::string& filename);
 
-    static void SaveEXR(const DeepImageBuffer& buf, const std::string filename);
+    static void SaveEXR(const DeepImageBuffer& buf, const std::string& filename);
 
-    static FlatImageBuffer LoadPPM(const std::string filename);
+    static FlatImageBuffer LoadPPM(const std::string& filename);
 
-    static DeepImageBuffer LoadEXR(const std::string filename);
+    static DeepImageBuffer LoadEXR(const std::string& filename);
 };
 
 }  // namespace skwr
