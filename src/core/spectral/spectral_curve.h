bb46cb23e984fdb035f56b6627ac09afa12feed1
Ian Yoo <isyoo@ucdavis.edu>
2026-02-20 01:05:40 -0800

    [feat] Spectral: add spectral curve and utility wrappers for rgb conversion

---
+++ src/core/spectral/spectral_curve.h
@@ -0,0 +1,12 @@
+#ifndef SKWR_CORE_SPECTRAL_SPECTRAL_CURVE_H_
+#define SKWR_CORE_SPECTRAL_SPECTRAL_CURVE_H_
+
+namespace skwr {
+
+struct SpectralCurve {
+    float coeff[3];
+};
+
+}  // namespace skwr
+
+#endif  // SKWR_CORE_SPECTRAL_SPECTRAL_CURVE_H_
