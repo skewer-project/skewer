85cecff05aafd0ef143e0b3b76b8242889cbb2f6
Ian Yoo <isyoo@ucdavis.edu>
2026-02-20 01:23:04 -0800

    [feat] WavelengthSampler: add hero wl sampler + stratification

---
+++ src/core/sampling/wavelength_sampler.h
@@ -0,0 +1,39 @@
+#ifndef SKWR_CORE_SAMPLING_WAVELENGTH_SAMPLER_H_
+#define SKWR_CORE_SAMPLING_WAVELENGTH_SAMPLER_H_
+
+#include "core/spectrum.h"
+
+namespace skwr {
+
+class WavelengthSampler {
+  public:
+    static constexpr float kLambdaMin = 360.0f;  // TODO: constants?
+    static constexpr float kLambdaMax = 830.0f;
+
+    static SampledWavelengths Sample(float u) {
+        SampledWavelengths wl;
+        const float range = kLambdaMax - kLambdaMin;
+        const float pdf = 1.0f / range;
+        const float delta = range / kNSamples;
+
+        // The "Hero" wavelength
+        wl.lambda[0] = kLambdaMin + u * range;
+        wl.pdf[0] = pdf;
+
+        // Stratify the other wavelengths (if kNSamples > 1)
+        for (int i = 1; i < kNSamples; ++i) {
+            float lambda = wl.lambda[0] + i * delta;
+            if (lambda > kLambdaMax) {
+                lambda -= range;  // Wrap around if we exceed the visible range
+            }
+            wl.lambda[i] = lambda;
+            wl.pdf[i] = pdf;
+        }
+
+        return wl;
+    }
+};
+
+}  // namespace skwr
+
+#endif  // SKWR_CORE_SAMPLING_WAVELENGTH_SAMPLER_H_
