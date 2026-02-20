11eddaed16ce4d6fdc8ada28dae5e8d805c32015
Ian Yoo <isyoo@ucdavis.edu>
2026-02-20 01:37:02 -0800

    [feat] PathTrace: add hero wavelength generation

--- src/integrators/path_trace.cc
+++ src/integrators/path_trace.cc
@@ -6,6 +6,8 @@
 
 #include "barkeep.h"
 #include "core/sampling.h"
+#include "core/sampling/wavelength_sampler.h"
+#include "core/spectrum.h"
 #include "film/film.h"
 #include "integrators/path_sample.h"
 #include "kernels/path_kernel.h"
@@ -57,12 +59,16 @@ void PathTrace::Render(const Scene& scene, const Camera& cam, Film* film,
                     float u = (float(x) + rng.UniformFloat()) / width;
                     float v = 1.0f - (float(y) + rng.UniformFloat()) / height;
 
+                    SampledWavelengths wl = WavelengthSampler::Sample(rng.UniformFloat());
+
                     Ray r = cam.GetRay(u, v);
 
-                    PathSample result = Li(r, scene, rng, config);
+                    PathSample result = Li(r, scene, rng, config, wl);
+
+                    RGB pixel_color = SpectrumToRGB(result.L, wl);
 
                     float weight = 1.0f;
-                    film->AddSample(x, y, result.L.ToRGB(), weight);
+                    film->AddSample(x, y, pixel_color, weight);
 
                     if (config.enable_deep) film->AddDeepSample(x, y, result, weight);
                 }
