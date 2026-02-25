9650aa215a3015b0379df33d6e28ab7fddbbcb09
C3viche <28328104+C3viche@users.noreply.github.com>
2026-02-24 21:38:44 -0800

    fix duplicated includes

--- src/core/spectral/spectral_utils.h
+++ src/core/spectral/spectral_utils.h
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <stdexcept>
 
-#include "../external/srgb_spec_data.h"
 #include "core/color.h"
 #include "core/spectral/rgb2spec.h"
 #include "core/spectral/spectral_curve.h"
