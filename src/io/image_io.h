b1b6c2b25134c1b7cd7e2e925a23e2c529dcb478
Ian Yoo <isyoo@ucdavis.edu>
2026-02-24 16:14:57 -0800

    [style] ImageIO: fix header naming

--- src/io/image_io.h
+++ src/io/image_io.h
@@ -1,5 +1,5 @@
-#ifndef IMAGE_IO_H
-#define IMAGE_IO_H
+#ifndef SKWR_IO_IMAGE_IO_H
+#define SKWR_IO_IMAGE_IO_H
 
 #include <string>
 
@@ -20,4 +20,4 @@ class ImageIO {
 
 }  // namespace skwr
 
-#endif
+#endif  // SKWR_IO_IMAGE_IO_H_
