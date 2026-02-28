00905241665e631218520c8c35e2d540bfd64f3c
C3viche <28328104+C3viche@users.noreply.github.com>
2026-02-27 23:54:11 -0800

    add locks around critical sections + store OriginalReq and FrameState

--- internal/coordinator/job.go
+++ internal/coordinator/job.go
@@ -18,6 +18,13 @@ type Job interface {
 	GetStatus() pb.GetJobStatusResponse_JobStatus
 	SetStatus(status pb.GetJobStatusResponse_JobStatus)
 	Progress() float32
+	GetOriginalReq() *pb.SubmitJobRequest
+}
+
+type FrameState struct {
+	CompletedChunks int32
+	TotalChunks     int32
+	PendingMerge    *pb.MergeTask // Hold the task here until it's ready
 }
 
 // The Render Job
@@ -30,19 +37,33 @@ type RenderJob struct {
 	CompletedTasks int32
 	TotalTasks     int32
 	SampleDivision int32
+
+	Frames map[string]*FrameState
+	mu     sync.Mutex // Protects the map during concurrent worker updates
+
+	OriginalReq *pb.SubmitJobRequest
 }
 
 func (rj *RenderJob) ID() string                                   { return rj.JobID } // Fulfills the DAGNode interface as well
 func (rj *RenderJob) GetDependencies() []string                    { return rj.Dependencies }
 func (rj *RenderJob) GetStatus() pb.GetJobStatusResponse_JobStatus { return rj.Status }
 func (rj *RenderJob) SetStatus(status pb.GetJobStatusResponse_JobStatus) {
+	rj.mu.Lock()
+	defer rj.mu.Unlock()
+
 	rj.Status = status
 }
 func (rj *RenderJob) Progress() float32 {
+	rj.mu.Lock()
+	defer rj.mu.Unlock()
+
 	if rj.TotalTasks == 0 {
 		return 0
 	}
 	return float32(rj.CompletedTasks) / float32(rj.TotalTasks)
+}
+func (rj *RenderJob) GetOriginalReq() *pb.SubmitJobRequest {
+	return rj.OriginalReq
 }
 
 // The Composite Job
@@ -54,19 +75,32 @@ type CompositeJob struct {
 	// Composite-specific fields
 	CompletedFrames int32
 	TotalFrames     int32
+
+	mu sync.Mutex
+
+	OriginalReq *pb.SubmitJobRequest
 }
 
 func (cj *CompositeJob) ID() string                                   { return cj.JobID } // Fulfills the DAGNode interface as well
 func (cj *CompositeJob) GetDependencies() []string                    { return cj.Dependencies }
 func (cj *CompositeJob) GetStatus() pb.GetJobStatusResponse_JobStatus { return cj.Status }
 func (cj *CompositeJob) SetStatus(status pb.GetJobStatusResponse_JobStatus) {
+	cj.mu.Lock()
+	defer cj.mu.Unlock()
+
 	cj.Status = status
 }
 func (cj *CompositeJob) Progress() float32 {
+	cj.mu.Lock()
+	defer cj.mu.Unlock()
+
 	if cj.TotalFrames == 0 {
 		return 0
 	}
 	return float32(cj.CompletedFrames) / float32(cj.TotalFrames)
+}
+func (rj *CompositeJob) GetOriginalReq() *pb.SubmitJobRequest {
+	return rj.OriginalReq
 }
 
 // ===================== //
@@ -88,17 +122,21 @@ type JobTracker struct {
 
 func (jt *JobTracker) AddJob(job Job) error {
 	// Check if job already exists
+	jt.mu.Lock()
+	defer jt.mu.Unlock()
+
 	if _, exists := jt.activeJobs[job.ID()]; exists {
 		return fmt.Errorf("[ERROR] Adding job that already exists with ID %s.", job.ID())
 	} else {
 		jobDeps := job.GetDependencies()
 
+		jt.activeJobs[job.ID()] = job // ALWAYS add it to active jobs!
+
 		// Add the job to the live tracker
 		if len(jobDeps) == 0 {
-			job.SetStatus(pb.GetJobStatusResponse_JOB_STATUS_PENDING_DEPENDENCIES)
-		} else {
 			job.SetStatus(pb.GetJobStatusResponse_JOB_STATUS_QUEUED)
-			jt.activeJobs[job.ID()] = job
+		} else {
+			job.SetStatus(pb.GetJobStatusResponse_JOB_STATUS_PENDING_DEPENDENCIES)
 		}
 		jt.pendingDeps[job.ID()] = len(jobDeps)
 
@@ -118,6 +156,9 @@ func (jt *JobTracker) AddJob(job Job) error {
 }
 
 func (jt *JobTracker) GetJob(jobID string) (Job, error) {
+	jt.mu.RLock() // Read lock is a bit faster
+	defer jt.mu.RUnlock()
+
 	job, exists := jt.activeJobs[jobID]
 	if !exists {
 		return nil, fmt.Errorf("[ERROR] Job with ID %s not found.", jobID)
@@ -126,6 +167,9 @@ func (jt *JobTracker) GetJob(jobID string) (Job, error) {
 }
 
 func (jt *JobTracker) CancelJob(jobID string) error {
+	jt.mu.Lock()
+	defer jt.mu.Unlock()
+
 	job, exists := jt.activeJobs[jobID]
 	if !exists {
 		return fmt.Errorf("[ERROR] Job with ID %s not found.", jobID)
