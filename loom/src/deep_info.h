e3ed163aaf7ac72613f051c9bfa7a080fdc7054e
shavolkov <shavolkov@gmail.com>
2026-03-02 16:31:10 -0800

    Clang format commit

--- loom/src/deep_info.h
+++ loom/src/deep_info.h
@@ -1,29 +1,28 @@
 #pragma once
 #include <OpenEXR/ImfDeepFrameBuffer.h>
-#include <OpenEXR/ImfDeepScanLineInputFile.h> // For reading deep EXR files
-#include <vector>
+#include <OpenEXR/ImfDeepScanLineInputFile.h>  // For reading deep EXR files
+
 #include <algorithm>
 #include <cmath>
-#include <stdexcept>
 #include <limits>
-
+#include <stdexcept>
+#include <vector>
 
 namespace deep_compositor {
 
-    
 class DeepInfo {
-public:   
+  public:
     DeepInfo();
-    DeepInfo(const std::string& filename) 
-        : file_(filename.c_str()) // This opens the file immediately
+    DeepInfo(const std::string& filename)
+        : file_(filename.c_str())  // This opens the file immediately
     {
         // Once the file is open, we extract the metadata (width/height)
         Imath::Box2i dw = file_.header().dataWindow();
-        width_  = dw.max.x - dw.min.x + 1;
+        width_ = dw.max.x - dw.min.x + 1;
         height_ = dw.max.y - dw.min.y + 1;
         printf("Loaded Deep EXR: %s (%dx%d)\n", filename.c_str(), width_, height_);
         printf("Number of parts in file: %d\n", file_.header().hasType());
-        // We can verify if it's deep, though DeepScanLineInputFile 
+        // We can verify if it's deep, though DeepScanLineInputFile
         // will throw an error if you point it at a non-deep file anyway.
     }
 
@@ -31,7 +30,7 @@ public:
     int height() const { return height_; }
     bool isDeep() const { return isDeep_; }
 
-    Imf::DeepScanLineInputFile& getFile()  { return file_; }
+    Imf::DeepScanLineInputFile& getFile() { return file_; }
 
     // Temporary buffer for sample counts of a single row
     const unsigned int* getSampleCountsForRow(int y) {
@@ -46,11 +45,11 @@ public:
      * - Configures a DeepFrameBuffer with a sample count slice pointing to the appropriate
      *   memory location using pointer arithmetic
      * - Reads only the sample counts (not the actual sample data) for the specified row
-     * 
+     *
      * The pointer arithmetic adjusts the base memory address by the total offset so that
      * the IMF library can correctly map pixel coordinates to buffer locations.
-     * 
- 
+     *
+
      */
     void fetchSampleCounts(int y) {
         // Resize buffer to fit one row of integers
@@ -60,24 +59,19 @@ public:
         int minX = dw.min.x;
 
         Imf::DeepFrameBuffer countBuffer;
-        // We point to the start of our vector, but tell OpenEXR 
+        // We point to the start of our vector, but tell OpenEXR
         // that this memory represents pixel (minX, y)
-        char* base = (char*)(tempSampleCounts.data()) 
-                    - (minX * sizeof(unsigned int)); 
-                    // Note: We don't subtract y because we only read one row (y, y)
+        char* base = (char*)(tempSampleCounts.data()) - (minX * sizeof(unsigned int));
+        // Note: We don't subtract y because we only read one row (y, y)
 
-        countBuffer.insertSampleCountSlice(Imf::Slice(
-            Imf::UINT, 
-            base, 
-            sizeof(unsigned int), // xStride
-            0                     // yStride (0 because we read 1 row)
-        ));
+        countBuffer.insertSampleCountSlice(Imf::Slice(Imf::UINT, base,
+                                                      sizeof(unsigned int),  // xStride
+                                                      0  // yStride (0 because we read 1 row)
+                                                      ));
 
         file_.setFrameBuffer(countBuffer);
         file_.readPixelSampleCounts(y, y);
     }
-
-
 
     // 2. Explicitly forbid Copying (Since the EXR file handle can't be duplicated)
     DeepInfo(const DeepInfo&) = delete;
@@ -86,13 +80,12 @@ public:
     // 3. Virtual destructor is good practice if you ever plan to inherit
     virtual ~DeepInfo() = default;
 
-
-private:
+  private:
     int width_;
     int height_;
 
     std::vector<unsigned int> tempSampleCounts;
-    
+
     Imf::DeepScanLineInputFile file_;
 
     bool isDeep_;
@@ -104,5 +97,4 @@ private:
     size_t index(int x, int y) const { return static_cast<size_t>(y) * width_ + x; }
 };
 
-
-}
+}  // namespace deep_compositor
