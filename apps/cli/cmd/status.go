2f6964d22d8f6ef75be82ee84d7a67eb198fd144
C3viche <28328104+C3viche@users.noreply.github.com>
2026-03-03 22:08:48 -0800

    add gemini-generated cobra cli config for user-facing cli

---
+++ apps/cli/cmd/status.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	coordinatorv1 "github.com/skewer-project/skewer/api/proto/coordinator/v1"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var statusJobID string
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Get the status of a submitted job",
+	Run: func(cmd *cobra.Command, args []string) {
+		conn, err := grpc.NewClient(coordinatorAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Did not connect: %v", err)
+		}
+		defer conn.Close()
+		c := coordinatorv1.NewCoordinatorServiceClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+
+		req := &coordinatorv1.GetJobStatusRequest{
+			JobId: statusJobID,
+		}
+
+		r, err := c.GetJobStatus(ctx, req)
+		if err != nil {
+			log.Fatalf("Could not get status: %v", err)
+		}
+
+		fmt.Printf("Job Status: %s\n", r.GetJobStatus().String())
+		fmt.Printf("Progress: %.1f%%\n", r.GetProgressPercent()*100)
+		if r.GetErrorMessage() != "" {
+			fmt.Printf("Error: %s\n", r.GetErrorMessage())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+
+	statusCmd.Flags().StringVarP(&coordinatorAddr, "coordinator", "c", "localhost:50051", "Address of the Skewer Coordinator")
+	statusCmd.Flags().StringVarP(&statusJobID, "job", "j", "", "The UUID of the job to check")
+	statusCmd.MarkFlagRequired("job")
+}
