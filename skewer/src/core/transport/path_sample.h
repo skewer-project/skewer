7562b61513ae41f162e6f1dd0a6488889646a044
Ian Yoo <isyoo@ucdavis.edu>
2026-03-05 23:43:32 -0800

    [refactor] PathSample: replace vector with bounded array

--- skewer/src/core/transport/path_sample.h
+++ skewer/src/core/transport/path_sample.h
@@ -1,8 +1,8 @@
 #ifndef SKWR_CORE_TRANSPORT_PATH_SAMPLE_H_
 #define SKWR_CORE_TRANSPORT_PATH_SAMPLE_H_
 
-#include <vector>
-
+#include "core/containers/bounded_array.h"
+#include "core/cpu_config.h"
 #include "core/spectral/spectrum.h"
 #include "core/transport/deep_segment.h"
 
@@ -11,10 +11,7 @@ namespace skwr {
 struct PathSample {
     Spectrum L;   // "Flat" beauty pass (premultiplied radiance)
     float alpha;  // Coverage for the flat beauty pass (0 = transparent, 1 = opaque)
-    std::vector<DeepSegment> segments;
-
-    // TODO: consider boost::small_vector or some optimization for stack vs heap alloc tradeoffs
-    explicit PathSample(size_t reserveCount = 16) : alpha(0.0f) { segments.reserve(reserveCount); }
+    BoundedArray<DeepSegment, kMaxDeepSegments> segments;
 };
 
 }  // namespace skwr
