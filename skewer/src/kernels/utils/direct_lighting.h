cdf47e1a453520334edbbdd44ed4d9dd4f3f2fc6
Ian Yoo <isyoo@ucdavis.edu>
2026-03-05 22:42:08 -0800

    [refactor] PathKernel: separate direct lighting calc from NEE

---
+++ skewer/src/kernels/utils/direct_lighting.h
@@ -0,0 +1,43 @@
+#ifndef SKWR_KERNELS_UTILS_DIRECT_LIGHTING_H_
+#define SKWR_KERNELS_UTILS_DIRECT_LIGHTING_H_
+
+#include "core/math/vec3.h"
+#include "core/sampling/rng.h"
+#include "core/spectral/spectral_utils.h"
+#include "core/spectral/spectrum.h"
+#include "scene/scene.h"
+
+namespace skwr {
+
+struct DirectLightSample {
+    Vec3 wi;            // Direction TO the light
+    float dist;         // Distance to the light
+    float pdf;          // Combined PDF (light selection + solid angle)
+    Spectrum emission;  // Unattenuated light emission
+};
+
+inline bool GenerateLightSample(const Vec3& origin, const Scene& scene, RNG& rng,
+                                const SampledWavelengths& wl, DirectLightSample* out_sample) {
+    if (!scene.Lights().empty()) return false;
+
+    int light_index = int(rng.UniformFloat() * scene.Lights().size());
+    const AreaLight& light = scene.Lights()[light_index];
+    LightSample ls = SampleLight(scene, light, rng);
+
+    Vec3 to_light = ls.p - origin;
+    float dist_sq = to_light.LengthSquared();
+    out_sample->dist = std::sqrt(dist_sq);
+    out_sample->wi = to_light / out_sample->dist;
+
+    float cos_light = std::fmax(0.0f, Dot(-out_sample->wi, ls.n));
+    float light_pdf_w = ls.pdf * dist_sq / cos_light;
+
+    out_sample->pdf = light_pdf_w * scene.InvLightCount();
+    out_sample->emission = CurveToSpectrum(ls.emission, wl);
+
+    return true;
+}
+
+}  // namespace skwr
+
+#endif  // SKWR_KERNELS_UTILS_DIRECT_LIGHTING_H_
