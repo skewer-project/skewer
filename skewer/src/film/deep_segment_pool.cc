8364be6f742fcaaec02e0711ad38d62f9893f010
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2026-03-07 01:03:13 -0800

    address concurrency issue for access to chunks_

--- skewer/src/film/deep_segment_pool.cc
+++ skewer/src/film/deep_segment_pool.cc
@@ -1,5 +1,7 @@
 #include "film/deep_segment_pool.h"
 
+#include <mutex>
+
 using namespace skwr;
 
 DeepSegmentPool::DeepSegmentPool(size_t initial_chunks) {
@@ -10,7 +12,7 @@ DeepSegmentPool::DeepSegmentPool(size_t initial_chunks) {
 }
 
 void DeepSegmentPool::GrowToFit(size_t chunk_index) {
-    std::lock_guard<std::mutex> lock(grow_mutex_);
+    std::unique_lock<std::shared_mutex> lock(chunks_mutex_);
     // Double-check after acquiring lock
     while (chunks_.size() <= chunk_index) {
         chunks_.push_back(std::make_unique<DeepSegmentNode[]>(kChunkSize));
@@ -20,16 +22,32 @@ void DeepSegmentPool::GrowToFit(size_t chunk_index) {
 size_t DeepSegmentPool::Allocate() {
     size_t index = cursor_.fetch_add(1, std::memory_order_relaxed);
     size_t chunk_index = index / kChunkSize;
-    if (chunk_index >= chunks_.size()) {
-        GrowToFit(chunk_index);
+
+    {
+        std::shared_lock<std::shared_mutex> lock(chunks_mutex_);
+        if (chunk_index < chunks_.size()) {
+            return index;
+        }
     }
+
+    GrowToFit(chunk_index);
     return index;
 }
 
+DeepSegmentNode* DeepSegmentPool::GetChunk(size_t chunk_index) {
+    std::shared_lock<std::shared_mutex> lock(chunks_mutex_);
+    return chunks_[chunk_index].get();
+}
+
+const DeepSegmentNode* DeepSegmentPool::GetChunk(size_t chunk_index) const {
+    std::shared_lock<std::shared_mutex> lock(chunks_mutex_);
+    return chunks_[chunk_index].get();
+}
+
 DeepSegmentNode& DeepSegmentPool::operator[](size_t index) {
-    return chunks_[index / kChunkSize][index % kChunkSize];
+    return GetChunk(index / kChunkSize)[index % kChunkSize];
 }
 
 const DeepSegmentNode& DeepSegmentPool::operator[](size_t index) const {
-    return chunks_[index / kChunkSize][index % kChunkSize];
+    return GetChunk(index / kChunkSize)[index % kChunkSize];
 }
