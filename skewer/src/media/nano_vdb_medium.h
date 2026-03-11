9cf5aa4a6d3185b8fb89f1a2ed5959ba7c4516ef
Ian Yoo <isyoo@ucdavis.edu>
2026-03-11 14:48:43 -0700

    fix tree and grid access

--- skewer/src/media/nano_vdb_medium.h
+++ skewer/src/media/nano_vdb_medium.h
@@ -10,16 +10,24 @@
 namespace skwr {
 
 struct NanoVDBMedium {
+    NanoVDBMedium(const NanoVDBMedium&) = delete;
+    NanoVDBMedium& operator=(const NanoVDBMedium&) = delete;
+
+    NanoVDBMedium(NanoVDBMedium&&) = default;
+    NanoVDBMedium& operator=(NanoVDBMedium&&) = default;
+
     SpectralCurve sigma_a_base;
     SpectralCurve sigma_s_base;
 
     float g;
+    float max_density = 1.0f;
     float density_multiplier = 1.0f;
 
     // The contiguous memory buffer holding the VDB data
     nanovdb::GridHandle<> handle;
     const nanovdb::FloatGrid* grid = nullptr;
     const nanovdb::FloatTree* tree = nullptr;
+    nanovdb::FloatTree::AccessorType accessor;
 
     // Fast marching bounds
     BoundBox bbox;
@@ -31,16 +39,36 @@ struct NanoVDBMedium {
             if (!grid) return false;
 
             tree = &grid->tree();
+            accessor = tree->getAccessor();
 
             // Extract the exact world-space bounding box
             auto vdb_bbox = grid->worldBBox();
             bbox = BoundBox(Vec3(vdb_bbox.min()[0], vdb_bbox.min()[1], vdb_bbox.min()[2]),
                             Vec3(vdb_bbox.max()[0], vdb_bbox.max()[1], vdb_bbox.max()[2]));
+
+            float min_val, max_val;
+            tree->extrema(min_val, max_val);
+            max_density = max_val * density_multiplier;  // perfect majorant
+
             return true;
         } catch (const std::exception& e) {
             std::cerr << "Failed to load NanoVDB: " << e.what() << "\n";
             return false;
         }
+    }
+
+    // Voxel lookup
+    float GetDensity(const Point3& p_world) const {
+        if (!grid) return 0.0f;
+
+        // Transform world-space ray coords into the VDB's internal 3D index space
+        nanovdb::Vec3f p_index =
+            grid->worldToIndexF(nanovdb::Vec3f(p_world.x(), p_world.y(), p_world.z()));
+
+        // Nearest-Neighbor Voxel Lookup
+        // TODO: Trilinear interpolation for smoother clouds maybe
+        float density = accessor.getValue(nanovdb::Coord::Floor(p_index));
+        return density * density_multiplier;
     }
 };
 
