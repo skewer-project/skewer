fb39b933b711ef02d728bc2cef4c852e734136d9
Ian Yoo <isyoo@ucdavis.edu>
2026-03-17 17:06:04 -0700

    [feat] SampleWriter: add multi pass, constructor, and flush

--- skewer/src/film/sample_writer.h
+++ skewer/src/film/sample_writer.h
@@ -10,21 +10,34 @@ namespace skwr {
 
 class SampleWriter {
   public:
+    SampleWriter(Film* film, int x, int y, float weight, bool is_adaptive, bool enable_deep)
+        : film_(film),
+          x_(x),
+          y_(y),
+          sample_weight_(weight),
+          is_adaptive_(is_adaptive),
+          enable_deep_(enable_deep) {}
+
+    inline void WriteBeauty(const RGB& L, float alpha) const {
+        if (is_adaptive_) {
+            film_->AddAdaptiveSample(x_, y_, L, alpha, sample_weight_);
+        } else {
+            film_->AddSample(x_, y_, L, alpha, sample_weight_);
+        }
+    }
+
+    // TODO: add AOV system, or individual WriteAlbedo, WriteNormals, etc
+    // inline void WriteAOV(int aov_type, const RGB& value) const {
+    //         // Assuming your film has an AOV system. If not, this is how you'd hook it up.
+    //         film->AddAOVSample(x, y, aov_type, value, sample_weight);
+    //     }
+
     inline void PushDeepSegment(float z_front, float z_back, const RGB& final_rgb, float alpha) {
         deep_segments_.push_back({z_front, z_back, final_rgb, alpha});
     }
 
-    inline void AddBeauty(const Spectrum& L, float alpha) const {
-        film_->AddBeautySample(pixelIndex, L, alpha);
-    }
-
-    inline void Commit(const RGB& beauty_L, float beauty_alpha) {
-        // 1. Commit Beauty
-        film_->AddSample(x_, y_, beauty_L, beauty_alpha, sample_weight);
-
-        // 2. Commit Deep (if any)
-        if (!deep_segments_.empty()) {
-            // We pass the local array to the film to handle the pool allocation and atomics
+    inline void FlushDeepSegments() {
+        if (enable_deep_ && !deep_segments_.empty()) {
             film_->AddDeepSample(x_, y_, deep_segments_);
         }
     }
