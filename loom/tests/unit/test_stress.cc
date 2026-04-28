dd25574fa1abdc822a4ff5ed02a500b21219c84f
C3viche <28328104+C3viche@users.noreply.github.com>
2026-04-27 20:53:21 -0700

    formatting

--- loom/tests/unit/test_stress.cc
+++ loom/tests/unit/test_stress.cc
@@ -13,24 +13,22 @@ namespace fs = std::filesystem;
 
 class StressTest : public ::testing::Test {
   protected:
-  // Simple options for testing a loom instance
-  Options simple_opts = {
-      .input_files = {},
-      .input_z_offsets = {},
-      .output_prefix = "",
-      .deep_output = false,
-      .flat_output = true,
-      .png_output = true,
-      .verbose = false,
-      .merge_threshold = 0.001f,
-      .show_help = false,
-      .mod_offset = false,
-      .enable_merging = true
-  };
+    // Simple options for testing a loom instance
+    Options simple_opts = {.input_files = {},
+                           .input_z_offsets = {},
+                           .output_prefix = "",
+                           .deep_output = false,
+                           .flat_output = true,
+                           .png_output = true,
+                           .verbose = false,
+                           .merge_threshold = 0.001f,
+                           .show_help = false,
+                           .mod_offset = false,
+                           .enable_merging = true};
 
-  fs::path current_file = __FILE__;
-  fs::path tests_dir = current_file.parent_path().parent_path();
-  fs::path assets_dir = tests_dir / "assets";
+    fs::path current_file = __FILE__;
+    fs::path tests_dir = current_file.parent_path().parent_path();
+    fs::path assets_dir = tests_dir / "assets";
 };
 
 // ============================================================================
@@ -39,7 +37,8 @@ class StressTest : public ::testing::Test {
 
 TEST_F(StressTest, VolumetricSplittingStress) {
     Options opts = simple_opts;
-    std::vector<std::string> input_files = {assets_dir / "layer_fog.exr", assets_dir / "layer_objects.exr"};
+    std::vector<std::string> input_files = {assets_dir / "layer_fog.exr",
+                                            assets_dir / "layer_objects.exr"};
     opts.input_files = input_files;
 
     std::vector<std::unique_ptr<deep_compositor::DeepInfo>> imagesInfo;
