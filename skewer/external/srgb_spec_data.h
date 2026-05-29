0d6f1ec4e8cb90a0016e69b569da8a23c8f3369f
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2026-05-29 13:45:40 -0700

    move srg_spec_data to skewer core as an extern const

--- skewer/external/srgb_spec_data.h
+++ skewer/external/srgb_spec_data.h
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d67fd3573980cbc1fd59af60068eb552559f4f8ddaa5e2463d20317468eaf90d
-size 58197790
+#ifndef RGB_SPEC_DATA_H_
+#define RGB_SPEC_DATA_H_
+
+extern const unsigned char srgb_spec[];
+extern const unsigned int srgb_spec_len;
+
+#endif
